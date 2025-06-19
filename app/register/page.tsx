ec1ad844f237919a9673e79d067e68ae5b82f659
shreyansh-singh74 <1032230662@tcetmumbai.in>
2025-06-20 00:17:16 +0530

    new

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,6 +4,8 @@ import React,{useState} from 'react'
 
 function RegisterPage() {
     const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
   return (
     <div>RegisterPage</div>
   )
